4da6c882bb4f65b35bd8868b4eda3f598843e1a2
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-06-15 20:34:41 +0600

    Add files via upload

---
+++ 502. IPO.cpp
@@ -0,0 +1,111 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+class Solution
+{
+public:
+    int findMaximizedCapital(int k, int w, vector<int>& profits,
+                             vector<int>& capital)
+    {
+        int n = profits.size();
+        vector<std::pair<int, int>> projects;
+        for (int i = 0; i < n; ++i)
+        {
+            projects.emplace_back(capital[i], profits[i]);
+        }
+
+        // Sorting projects by capital required
+        sort(projects.begin(), projects.end());
+
+        // Max-heap to store profits, using greater to create a max-heap
+        priority_queue<int> maxHeap;
+        int i = 0;
+
+        // Main loop to select up to k projects
+        for (int j = 0; j < k; ++j)
+        {
+            // Add all profitable projects that we can afford
+            while (i < n && projects[i].first <= w)
+            {
+                maxHeap.push(projects[i].second);
+                i++;
+            }
+
+            // If no projects can be funded, break out of the loop
+            if (maxHeap.empty())
+            {
+                break;
+            }
+
+            // Otherwise, take the project with the maximum profit
+            w += maxHeap.top();
+            maxHeap.pop();
+        }
+
+        return w;
+    }
+};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+//        solve();
+
+    }
+    return 0;
+}
