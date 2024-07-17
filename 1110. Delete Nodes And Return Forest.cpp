6e3a85cc056991ca9cc80bb8f362436a5655d575
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-07-17 08:35:35 +0600

    Add files via upload

---
+++ 1110. Delete Nodes And Return Forest.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+//class Solution
+//{
+//public:
+//    TreeNode* deleteHelper(TreeNode* root, unordered_set<int>& st, vector<TreeNode*>& result)
+//    {
+//        if(root == NULL)
+//        {
+//            return NULL;
+//        }
+//        root->left  = deleteHelper(root->left, st, result);
+//        root->right = deleteHelper(root->right, st, result);
+//
+//        if(st.find(root->val) != st.end())
+//        {
+//            if(root->left != NULL)
+//            {
+//
+//                result.push_back(root->left);
+//            }
+//
+//            if(root->right != NULL)
+//            {
+//                result.push_back(root->right);
+//            }
+//
+//            return NULL;
+//        }
+//        else
+//        {
+//            return root;
+//        }
+//    }
+//    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete)
+//    {
+//        vector<TreeNode*> result;
+//
+//        unordered_set<int> st;
+//
+//        for(int &num : to_delete)
+//        {
+//            st.insert(num);
+//        }
+//
+//        deleteHelper(root, st, result);
+//        if(st.find(root->val) == st.end())
+//        {
+//            result.push_back(root);
+//        }
+//        return result;
+//    }
+//};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+//        solve();
+
+    }
+    return 0;
+}
