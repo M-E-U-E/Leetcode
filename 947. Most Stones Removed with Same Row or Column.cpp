dbac8344bdc73c6225bb4c5beec117bfd1799305
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-08-29 18:55:46 +0600

    Add files via upload

---
+++ 947. Most Stones Removed with Same Row or Column.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+class Solution
+{
+private:
+    int connectedComponentCount = 0;
+
+    int findRepresentative(int element, vector<int>& setRepresentatives)
+    {
+        if (setRepresentatives[element] == 0)
+        {
+            setRepresentatives[element] = element;
+            connectedComponentCount++;
+        }
+        return setRepresentatives[element] == element ? element :
+               (setRepresentatives[element] = findRepresentative(setRepresentatives[element], setRepresentatives));
+    }
+    void mergeComponents(int elementA, int elementB, vector<int>& setRepresentatives)
+    {
+        int repA = findRepresentative(elementA, setRepresentatives);
+        int repB = findRepresentative(elementB, setRepresentatives);
+        if (repA != repB)
+        {
+            setRepresentatives[repB] = repA;
+            connectedComponentCount--;
+        }
+    }
+public:
+    int removeStones(vector<vector<int>>& stonePositions)
+    {
+        vector<int> setRepresentatives(20003, 0);
+        for (const auto& stonePosition : stonePositions)
+        {
+            mergeComponents(stonePosition[0] + 1, stonePosition[1] + 10002, setRepresentatives);
+        }
+        return stonePositions.size() - connectedComponentCount;
+    }
+};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+//        solve();
+
+    }
+    return 0;
+}
