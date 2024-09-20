1f943440bdf4c02c7bfa1a2606b8ba9339b10927
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-09-20 12:40:49 +0600

    Add files via upload

---
+++ 214. Shortest Palindrome.cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+class Solution
+{
+public:
+    string shortestPalindrome(string s)
+    {
+        const int n = s.size();
+        int i = 0;
+        for (int j=n-1; j>= 0; j--)
+        {
+            while (j>=0 && s[i] == s[j])
+            {
+                i++, j--;
+            }
+        }
+        if (i==n)
+        {
+            return s;
+        }
+        string sub= s.substr(i), remain_rev=sub;
+        reverse(remain_rev.begin(), remain_rev.end());
+        return remain_rev + shortestPalindrome(s.substr(0, i)) + sub;
+    }
+};
+auto init = []()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}
+();
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+//        solve();
+
+    }
+    return 0;
+}
