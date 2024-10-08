51d9b7f30640429e9c99935d963a4d925a4d33a9
Md. Emtiyaj Uddin Emon <137956365+M-E-U-E@users.noreply.github.com>
2024-10-08 21:31:09 +0600

    Add files via upload

---
+++ 2696. Minimum String Length After Removing Substrings.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define endl "\n"
+using namespace std;
+bool cmp(pair<int,int>a,pair<int,int> b)
+{
+    if(a.first > b.first)
+        return true;
+    else if(a.first == b.first and a.second<b.second)
+        return true;
+
+    return false;
+}
+//vector<ll>v;
+//void primeFactors(ll n)
+//{
+//    // Print the number of 2s that divide n
+//    while (n % 2 == 0)
+//    {
+//        v.push_back(2);
+//        n = n/2;
+//    }
+//
+//    // n must be odd at this point. So we can skip
+//    // one element (Note i = i +2)
+//    for (ll i = 3; i*i<=n; i = i + 2)
+//    {
+//        // While i divides n, print i and divide n
+//        while (n % i == 0)
+//        {
+//            // cout<<i<<ss;
+//            v.push_back(i);
+//            n = n/i;
+//        }
+//    }
+//
+//    // This condition is to handle the case when n
+//    // is a prime number greater than 2
+//    if (n > 2)
+//        v.push_back(n);
+//}
+const int N = 1e5+10;
+bitset <N> vis;
+void sieve (void)
+{
+    for (int i = 2; i * i < N; ++i)
+    {
+        if (!vis[i])
+        {
+            for (int j = i * i; j < N; j += i) vis[j] = 1;
+        }
+    }
+}
+class Solution
+{
+public:
+    int minLength(string s)
+    {
+        stack<char> stack;
+        for (int i = 0; i < s.length(); i++)
+        {
+            char cur_char = s[i];
+            if (stack.empty())
+            {
+                stack.push(cur_char);
+                continue;
+            }
+            if(cur_char == 'B' && stack.top() == 'A')
+            {
+                stack.pop();
+            }
+            else if(cur_char == 'D' && stack.top() == 'C')
+            {
+                stack.pop();
+            }
+            else
+            {
+                stack.push(cur_char);
+            }
+        }
+        return stack.size();
+    }
+};
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+//        solve();
+
+    }
+    return 0;
+}
